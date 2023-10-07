afa43d26c754f54d7caf6d1fc56166c45b492d9e
Being Of Existence <manfromearth25@gmail.com>
2023-10-07 09:50:16 +0000

    feat: hello

--- setup.sh
+++ setup.sh
@@ -10,3 +10,4 @@ sudo apt-get update \
         fonts-droid-fallback vim-tiny nano libgconf2-dev libgtk-3-dev twm \
     && sudo apt-get clean && sudo rm -rf /var/cache/apt/* && sudo rm -rf /var/lib/apt/lists/* && sudo rm -rf /tmp/*
 
+ 
